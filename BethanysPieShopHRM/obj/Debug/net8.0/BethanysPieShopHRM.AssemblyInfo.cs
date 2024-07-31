2f511404d75fe70c070bc47bd63eb82f0aae0004
AaronRamirez05 <aaronramirez879@yahoo.com>
2024-07-30 20:49:22 -0500

    Part 4 finished

--- BethanysPieShopHRM/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
+++ BethanysPieShopHRM/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e087fc7378db68093ffe774a2e65812ee9662a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
